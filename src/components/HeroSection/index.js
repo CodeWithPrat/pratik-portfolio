eee5040e7506b9146ac0c0c7427fc17156e4be83
PRATIK <pratiktillekar57@gmail.com>
2024-02-04 09:38:57 +0530

    first commit
    this is my portfolio

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,78 @@
+import React from "react";
+import HeroBgAnimation from "../HeroBgAnimation";
+import {
+    HeroContainer,
+    HeroBg,
+    HeroLeftContainer,
+    Img,
+    HeroRightContainer,
+    HeroInnerContainer,
+    TextLoop,
+    Title,
+    Span,
+    SubTitle,
+    SocialMediaIcons,
+    SocialMediaIcon,
+    ResumeButton,
+    DownloadResumeButton,
+} from "./HeroStyle";
+import HeroImg from "../../images/pratik_rmbg.png";
+import Typewriter from "typewriter-effect";
+import { Bio } from "../../data/constants";
+
+const HeroSection = () => {
+    const handleDownloadResume = () => {
+        // Replace 'YOUR_FILE_ID' with the actual Google Drive file ID of your resume file
+        const resumeFileId = "1xl3jU-zXBa5oiPlhlMRttW4juLqlTwQO";
+
+        const directLink = `https://drive.google.com/uc?export=download&id=${resumeFileId}`;
+
+        const anchor = document.createElement("a");
+        anchor.href = directLink;
+        anchor.download = "pratik_9902827441.pdf"; // Replace with the desired filename
+        anchor.click();
+    };
+
+    return (
+        <div id="about">
+            <HeroContainer>
+                <HeroBg>
+                    <HeroBgAnimation />
+                </HeroBg>
+                <HeroInnerContainer>
+                    <HeroLeftContainer id="Left">
+                        <Title>
+                            Hi, I am <br /> {Bio.name}
+                        </Title>
+                        <TextLoop>
+                            I am a
+                            <Span>
+                                <Typewriter
+                                    options={{
+                                        strings: Bio.roles,
+                                        autoStart: true,
+                                        loop: true,
+                                    }}
+                                />
+                            </Span>
+                        </TextLoop>
+                        <SubTitle>{Bio.description}</SubTitle>
+                        <div style={{ display: "flex", gap: "10px" }}>
+                            <ResumeButton href={Bio.resume} target="display">
+                                Check Resume
+                            </ResumeButton>
+                            <DownloadResumeButton onClick={handleDownloadResume}>
+                                Download Resume
+                            </DownloadResumeButton>
+                        </div>
+                    </HeroLeftContainer>
+                    <HeroRightContainer id="Right">
+                        <Img src={HeroImg} alt="hero-image" />
+                    </HeroRightContainer>
+                </HeroInnerContainer>
+            </HeroContainer>
+        </div>
+    );
+};
+
+export default HeroSection;
---
+++ src/data/constants.js
@@ -0,0 +1,383 @@
+
+export const Bio = {
+  name: "PRATIK",
+  roles: [
+    "Java Full Stack Developer",
+    "Programmer",
+  ],
+  description:
+    "Welcome to my portfolio! I'm a dedicated Java Full Stack Developer with expertise in backend and frontend technologies. Proficient in Java, I excel in crafting scalable web applications, emphasizing clean code and efficient solutions. My skills extend to OOP, HTML, CSS, JavaScript, SQL databases, JDBC, and frameworks like Spring, Spring Boot, Hibernate, and Java MVC. Committed to staying current with industry trends, I bring a collaborative problem-solving approach to drive excellence in software development. Eager to contribute to innovative projects, I am focused on achieving success as a Java Full Stack Developer",
+  github: "https://github.com/CodeWithPrat",
+  resume:
+    "https://drive.google.com/file/d/1xl3jU-zXBa5oiPlhlMRttW4juLqlTwQO/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/pratik-169b55199/",
+  twitter: "https://twitter.com/javazzCode",
+  insta: "https://www.instagram.com/pratiiiiiiiiiiiiiikkk/",
+  facebook: "https://www.facebook.com/im.pratik57",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "Next Js",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      }
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Spring Boot",
+        image: "https://raw.githubusercontent.com/CodeWithPrat/portfolioImages/247874365bac37dad9a4940d804f68aa7cf1dc34/icons8-spring-boot%20(2).svg",
+      },
+      {
+        name: "Hibernate",
+        image: "https://hibernate.org/images/hibernate-logo.svg",
+      },
+      {
+        name: "Spring",
+        image: "https://raw.githubusercontent.com/CodeWithPrat/portfolioImages/ac78d5773afa46e8802bcb3694668184406c62ba/icons8-spring-boot%20(1).svg",
+      },
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image: "https://upload.wikimedia.org/wikipedia/commons/6/64/Expressjs.png",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "Android",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "XML",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/1024px-Git_icon.svg.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg",
+      }
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://www.kodnest.com/wp-content/uploads/2022/12/3.png",
+    role: "Java Full-Stack Developer Intern",
+    company: "KodNest",
+    date: "July 2023 - Present",
+    desc: "During my internship as a Java Full Stack Developer, I immersed myself in comprehensive training, gaining hands-on experience in the end-to-end development of web applications. This journey equipped me with a robust skill set, covering Java, Spring Boot, Hibernate, MySQL, HTML, CSS, JavaScript, and React. In frontend development, I crafted responsive interfaces, collaborating with UX/UI designers. On the backend, I developed scalable solutions, implementing RESTful APIs and excelling in database management with MySQL. Proficient in Git for version control, my internship not only enhanced my technical capabilities but also fostered a collaborative and result-oriented mindset. Eager to leverage these skills, I look forward to contributing to innovative projects and continuing my growth as a Java Full Stack Developer.",
+    skills: [
+      "Java",
+      "Object Oriented Programming",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "SQL Database",
+      "JDBC",
+      "Spring",
+      "spring boot",
+      "Hibernate",
+      "Java MVC"
+    ],
+    doc: "",
+  },
+  {
+    id: 1,
+    img: "https://media.licdn.com/dms/image/C511BAQFB7W5mPy4x2w/company-background_10000/0/1584114330626/inventeron_technologies_cover?e=2147483647&v=beta&t=8DQUFo2yLZyHJAJpUTSxEgOahUGIB39vDe7nfgWrDtQ",
+    role: "AIML Intern",
+    company: "Inventeron Technologies and Business Solutions LLP",
+    date: "Oct 2022 - Nov 2022",
+    desc: "Worked as an intern in an Artificial Intelligence and Machine Learning (AIML) team, where gained hands-on experience that directly contributed to a 20% improvement in model accuracy for a critical AIML project. Assisted in data preprocessing, feature engineering, and model training, resulting in a 25% reduction inmodel training time and a 15% increase in model accuracy, thereby contributing to the overall success of AIprojects.",
+    skills: [
+      "Artificial Intelligence",
+      "Machine Learning",
+      "Data collection and preprocessing",
+      "PyTorch",
+      "TensorFlow",
+      "Pandas",
+      "MySQL",
+    ],
+    doc: "https://drive.google.com/file/d/17DqF-FXk4sVAsuzmnB4AUy2uUkxcORtH/view?usp=sharing",
+  }
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://upload.wikimedia.org/wikipedia/en/b/bf/Acharya_Institutes_Logo.jpg",
+    school: "Acharya Institute of Technology, Bengaluru",
+    date: "Aug 2019 - June 2023",
+    grade: "7.58 CGPA",
+    desc: "I have successfully attained a Bachelor's degree in Computer Science and Engineering from Acharya Institute of Technology, Bengaluru, showcasing four years of dedicated academic pursuit. My academic journey has been marked by a commendable CGPA of 7.58. Throughout this enriching experience, I delved into comprehensive courses covering a spectrum of crucial subjects, including Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others",
+    degree: "Bachelor of Technology - B.E., Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://scontent.fblr20-2.fna.fbcdn.net/v/t31.18172-8/333370_459177067451677_1369489318_o.jpg?_nc_cat=106&ccb=1-7&_nc_sid=7a1959&_nc_ohc=HexsxmX49SYAX-pxGOb&_nc_ht=scontent.fblr20-2.fna&oh=00_AfBxNHD9Jg0h7qZqSHxXx2S5_CxT5G8j1Ho_avYcBZ_cVQ&oe=65D0C784",
+    school: "Sarvajna PU College of Science, Kalaburagi, Karnataka - 585105",
+    date: "April 2017 - Feb 2019",
+    grade: "84%",
+    desc: "During this academic journey, I immersed myself in a comprehensive curriculum that included subjects like Physics, Chemistry, Mathematics, and Biology. The enriching learning environment and challenging coursework at Sarvajna PU College have significantly contributed to my academic foundation and overall growth",
+    degree: "PU, Science - PCMB",
+  },
+  {
+    id: 2,
+    img: "https://raw.githubusercontent.com/CodeWithPrat/portfolioImages/main/vishwabharati.jpg",
+    school: "Vishwabharati Primary And High School , Humnabad, Dist. Bidar, Karnataka - 585330",
+    date: "Apr 2017 ",
+    grade: "91.04%",
+    desc: "I successfully completed my SSLC education at the esteemed Vishwabharati Primary and High School in Humnabad (Bidar). My time there not only equipped me with a strong academic foundation but also instilled in me essential life skills and a passion for learning. The supportive environment and dedicated educators played a crucial role in shaping my formative years, fostering a love for knowledge that continues to drive my educational journey",
+    degree: "SSLC, Kannada",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Expense Tracker App",
+    date: "June 2022",
+    description:
+      "The Expense Tracker App is a user-friendly mobile application designed to help individuals manage their finances effectively. With a simple and intuitive interface, users can easily record and categorize their expenses, set budget limits, and track their spending habits.",
+    image:
+      "https://assets.gqindia.com/photos/608c05836e6a489a62cd2549/master/w_1600%2Cc_limit/walnut-app.jpg",
+    tags: ["Java", "XML", "Android Studio"],
+    category: "android app",
+    github: "https://github.com/CodeWithPrat/ExpenseTrackerApp",
+    webapp: "https://github.com/CodeWithPrat/ExpenseTrackerApp",
+    member: [
+      {
+        name: "PRATIK",
+        img: "https://raw.githubusercontent.com/CodeWithPrat/portfolioImages/main/pratik.jpeg",
+        linkedin: "https://www.linkedin.com/in/pratik-169b55199/",
+        github: "https://github.com/CodeWithPrat",
+      },
+      {
+        name: "Hiten Patadiya",
+        img: "https://raw.githubusercontent.com/CodeWithPrat/portfolioImages/main/hiten.png",
+        linkedin: "https://www.linkedin.com/in/hiten-patadiya-4bb0a5202/",
+        github: "",
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "Heart disease detection Based on Machine Learning (Deep-CNN) from ECG signal",
+    date: "May 2023",
+    description:
+      "Developing a heart disease detection system using Deep Convolutional Neural Networks (Deep-CNN) involves training the model on electrocardiogram (ECG) signals to identify patterns indicative of cardiac abnormalities. The model learns to extract features from the ECG data, enabling it to distinguish between normal and abnormal heart rhythms. By leveraging the power of deep learning, this approach enhances the accuracy of early detection and classification of heart diseases, aiding in timely medical intervention. Implementation of such systems holds promise for improving the efficiency of cardiovascular healthcare through automated and precise diagnostics",
+    image:
+      "https://pub.mdpi-res.com/mathematics/mathematics-11-00562/article_deploy/html/images/mathematics-11-00562-ag.png?1675327591",
+    tags: ["DNN", "CNN", "ML"],
+    category: "machine learning",
+    github: "",
+    webapp: "",
+    member: [
+      {
+        name: "PRATIK",
+        img: "https://raw.githubusercontent.com/CodeWithPrat/portfolioImages/main/pratik.jpeg",
+        linkedin: "https://www.linkedin.com/in/pratik-169b55199/",
+        github: "https://github.com/CodeWithPrat",
+      },
+      {
+        name: "Hiten Patadiya",
+        img: "https://raw.githubusercontent.com/CodeWithPrat/portfolioImages/main/hiten.png",
+        linkedin: "https://www.linkedin.com/in/hiten-patadiya-4bb0a5202/",
+        github: "",
+      },
+      {
+        name: "C M Anusha",
+        img: "https://raw.githubusercontent.com/CodeWithPrat/portfolioImages/main/anusha.png",
+        linkedin: "",
+        github: "",
+      },
+      {
+        name: "SHIVUKUMAR",
+        img: "https://raw.githubusercontent.com/CodeWithPrat/portfolioImages/main/vishnu.png",
+        linkedin: "",
+        github: "",
+      },
+
+    ],
+  },
+  {
+    id: 2,
+    title: "Sales Prediction Using Linear Regression",
+    date: "Oct 2022",
+    description:
+      "The Sales Prediction Using Linear Regression is a data-driven project aimed at helping businesses forecast their sales based on historical data. By applying the principles of linear regression, this project provides a predictive model that allows businesses to make informed decisions and plan their strategies more effectively.",
+    image:
+      "https://miro.medium.com/v2/resize:fit:1400/0*L5RWrgC3N2gKISj3.png",
+    tags: [
+      "PyCharm",
+      "Python",
+      "Data Science",
+    ],
+    category: "machine learning",
+    github: "https://github.com/CodeWithPrat/SalesPredictionusingLinearRegression",
+    webapp: "https://github.com/CodeWithPrat/SalesPredictionusingLinearRegression",
+  },
+  {
+    id: 3,
+    title: "Predicting College University using Logistic Regression",
+    date: "Oct 2022",
+    description:
+      "The Predicting College University using Logistic Regression is a machine learning project that classifies student admission to colleges based on academic performance and extracurricular activities. It aids data-driven admission decisions, identifying key factors and predicting student outcomes accurately.",
+    image:
+      "https://images.spiceworks.com/wp-content/uploads/2022/04/11040522/46-4.png",
+    tags: ["Python", "Keras", "TensorFlow", "Pandas", "PyCharm"],
+    category: "machine learning",
+    github: "https://github.com/CodeWithPrat/Predicting_College_University_Using_LogisticRegression",
+    webapp: "https://github.com/CodeWithPrat/Predicting_College_University_Using_LogisticRegression",
+  },
+  {
+    id: 4,
+    title: "Stocks Prediction Using Neural Network",
+    date: "Nov 2022",
+    description:
+      "The Stocks Prediction Using Neural Network is a data-driven project that employs neural networks to forecast stock prices. This project uses historical stock data, market indicators, and other relevant features to train the neural network model. The goal is to predict future stock prices accurately, aiding investors in making informed decisions",
+    image:
+      "https://www.researchgate.net/publication/340698512/figure/fig1/AS:881350466867202@1587141734707/Detailed-architecture-of-Artificial-Neural-Network-ANN-for-stock-price-prediction.png",
+    tags: ["Python", "Keras", "TensorFlow", "Pandas", "PyCharm"],
+    category: "machine learning",
+    github: "https://github.com/CodeWithPrat/Stocks_prediction_using_neural_network",
+    webapp: "https://github.com/CodeWithPrat/Stocks_prediction_using_neural_network",
+  },
+
+  {
+    id: 5,
+    title: "School Management System",
+    date: "Feb 2022 - Apr 2022",
+    description:
+      "The School Management System using SQL is an efficient software solution for educational institutions. It manages student information, attendance, performance, and more in a robust SQL database. With course management, fee tracking, library organization, and reporting tools",
+    image:
+      "https://5.imimg.com/data5/SELLER/Default/2022/11/GM/XP/NB/18962817/school-management-software-500x500.jpg",
+    tags: ["HTML", "CSS", "JavaScript", "PHP", "Jquery", "MySQL"],
+    category: "web app",
+    github: "https://github.com/CodeWithPrat/SchoolManagementSystem",
+    webapp: "https://github.com/CodeWithPrat/SchoolManagementSystem",
+  },
+  {
+    id: 6,
+    title: "Employee Management System",
+    date: "Nov 2021",
+    description:
+      "The Employee Management System using SQL is a comprehensive and user-friendly software solution designed to streamline and automate various HR-related tasks within an organization. It is built upon a robust SQL database, offering efficient data storage, retrieval, and management capabilities.",
+    image:
+      "https://www.sourcecodester.com/sites/default/files/images/Hadiprasetyo/management_data_employe.jpg",
+    tags: ["HTML", "CSS", "JavaScript", "PHP", "Jquery", "MySQL"],
+    category: "web app",
+    github: "https://github.com/CodeWithPrat/EmployeeManagementSystem",
+    webapp: "https://github.com/CodeWithPrat/EmployeeManagementSystem",
+    member: [
+      {
+        name: "PRATIK",
+        img: "https://raw.githubusercontent.com/CodeWithPrat/portfolioImages/main/pratik.jpeg",
+        linkedin: "https://www.linkedin.com/in/pratik-169b55199/",
+        github: "https://github.com/CodeWithPrat",
+      },
+      {
+        name: "Rakshitha R",
+        img: "https://raw.githubusercontent.com/CodeWithPrat/portfolioImages/main/rakshitha.png",
+        linkedin: "",
+        github: "",
+      },
+    ],
+  },
+];
+
+export const TimeLineData = [
+  { year: 2019, text: "Started my journey" },
+  { year: 2022, text: "Shared my projects with the world" },
+  { year: 2023, text: "Completed my bachelor's degree" },
+  { year: 2024, text: "Completed Java full stack development internship" },
+];
